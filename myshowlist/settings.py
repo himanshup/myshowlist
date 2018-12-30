e1a177425824c1007e1e019008a7e9cc030366b8
himanshup <himanshup@csu.fullerton.edu>
2018-12-30 04:55:45 -0800

    add Elastic Beanstalk database config and requirements files

--- myshowlist/settings.py
+++ myshowlist/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Tells Django to place static files in our app's dir under www/static
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -74,12 +76,24 @@ WSGI_APPLICATION = 'myshowlist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+if 'RDS_DB_NAME' in os.environ:
+  DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': os.environ['RDS_DB_NAME'],
+          'USER': os.environ['RDS_USERNAME'],
+          'PASSWORD': os.environ['RDS_PASSWORD'],
+          'HOST': os.environ['RDS_HOSTNAME'],
+          'PORT': os.environ['RDS_PORT'],
+      }
+  }
+else:
+  DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      }
+  }
 
 
 # Password validation
