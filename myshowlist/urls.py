925b7c4db4e543644dda06e2e92fcdde05269fd3
himanshup <himanshup@csu.fullerton.edu>
2018-12-30 05:45:03 -0800

    add line to import obtain_jwt_token

--- myshowlist/urls.py
+++ myshowlist/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
