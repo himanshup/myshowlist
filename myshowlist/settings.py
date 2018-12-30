ce9d7933c7e77557d02388061b7afec859965249
himanshup <himanshup@csu.fullerton.edu>
2018-12-30 05:30:38 -0800

    add rest framework and local db config to settings

--- myshowlist/settings.py
+++ myshowlist/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'api'
 ]
 
@@ -90,8 +91,12 @@ if 'RDS_DB_NAME' in os.environ:
 else:
   DATABASES = {
       'default': {
-          'ENGINE': 'django.db.backends.sqlite3',
-          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'kitsu-clone',
+          'USER': 'postgres',
+          'PASSWORD': '',
+          'HOST': 'localhost',
+          'PORT': '5432'
       }
   }
 
