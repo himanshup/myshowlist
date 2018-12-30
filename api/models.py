7a11b564d1832783c79630dc1ff31d861e55fceb
himanshup <himanshup@csu.fullerton.edu>
2018-12-30 05:29:38 -0800

    add models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Entry(models.Model):
+  malID = models.IntegerField()
+  title = models.CharField(max_length=255)
+  synopsis = models.TextField()
+  image = models.CharField(max_length=255)
+  year = models.CharField(max_length=255)
+  rating = models.IntegerField()
+  malRating = models.FloatField()
+  episodes = models.IntegerField()
+  progress = models.IntegerField()
+  status = models.CharField(max_length=255)
+  author = models.ForeignKey(
+      'auth.User', related_name='entries', on_delete=models.CASCADE)
