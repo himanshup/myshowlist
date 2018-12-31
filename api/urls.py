1850460baa201aecfeb6792e4a75665927f1b696
himanshup <himanshup@csu.fullerton.edu>
2018-12-30 19:35:35 -0800

    update api url endpoints to look cleaner

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import UserDetail, LoginView, RegisterView, ListView, EntryDetail, LogoutView
+from .views import UserDetail, LoginView, RegisterView, LogoutView, ListView, EntryDetail, ShowsView, ShowDetail
 
 
 urlpatterns = [
     path('users/<int:pk>/', UserDetail.as_view()),
-    path('list/', ListView.as_view()),
-    path('list/<int:pk>/<str:status>/<str:sort>/<str:direction>/', ListView.as_view()),
-    path('entry/<int:pk>/', EntryDetail.as_view()),
+    path('users/<int:pk>/list/', ListView.as_view()),
+    path('users/<int:userId>/entry/<int:pk>/', EntryDetail.as_view()),
+    path('shows/', ShowsView.as_view()),
+    path('shows/<int:pk>/', ShowDetail.as_view()),
     path('auth/login/', LoginView.as_view()),
     path('auth/logout/', LogoutView.as_view()),
     path('auth/register/', RegisterView.as_view())
